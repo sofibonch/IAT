0724fde089703343f542968d4d9a60aefdb83b84
sofibonch <84900142+sofibonch@users.noreply.github.com>
2022-04-03 09:44:00 +0300

    Update iat.js

--- iat.js
+++ iat.js
@@ -40,7 +40,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 			{
 			name : 'Democrats', //Attribute label
 			title : {
-				media : {word : 'Positive'}, //Name of the category presented in the task.
+				media : {word : 'Democarats'}, //Name of the category presented in the task.
 				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
